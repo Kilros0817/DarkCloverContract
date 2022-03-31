6439dccc6ebe4c79999c9cfc99d6a8bea135a731
Kilros0817 <lazarkitfrl@gmail.com>
2022-04-01 01:13:47 +0300

    turn back deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,45 +1,45 @@
 async function main() {
   const team_address = "0x24be59F617ff5B93528F1471b80c1592eFfdF423";
   const marketing_address = "0x6B630A52F5Ec882A78B504065AED16a8C704c609";
-  const seedFT_address = "0xf53804452D68d79Ca26FEE411EE58a8c41cAC73c"
+  // const seedFT_address = "0x1969ae662f6cfC5C2220f6687b2C03FaE1025734"
   // const seedNFT_address = "0x9F46Aeb26b37921574b9C614Ce869D1d481cCE9c"
   // const seedController_address = "0xC1FA1087f8b5e1242028Dce7d257d48579e807b1"
   // const seedPicker_address = "0xc5199467A5e1cBC19ecfd0Bb6CFEfFaE9a1349f8"
 
 
-  // const seedFT = await ethers.getContractFactory("CloverDarkSeedToken");
-  // const seedNFT = await ethers.getContractFactory("CloverDarkSeedNFT");
-  // const seedController = await ethers.getContractFactory("CloverDarkSeedController");
-  // const seedPicker= await ethers.getContractFactory("CloverDarkSeedPicker");
-  // const seedStake= await ethers.getContractFactory("CloverDarkSeedStake" ,{
-  //   libraries: {
-  //     IterableMapping: "0xdfE52d83395cdB4276E7118cdd90d4f915888Cb6",
-  //   }
-  // });
+  const seedFT = await ethers.getContractFactory("CloverDarkSeedToken");
+  const seedNFT = await ethers.getContractFactory("CloverDarkSeedNFT");
+  const seedController = await ethers.getContractFactory("CloverDarkSeedController");
+  const seedPicker= await ethers.getContractFactory("CloverDarkSeedPicker");
+  const seedStake= await ethers.getContractFactory("CloverDarkSeedStake" ,{
+    libraries: {
+      IterableMapping: "0xdfE52d83395cdB4276E7118cdd90d4f915888Cb6",
+    }
+  });
   const seedPotion= await ethers.getContractFactory("CloverDarkSeedPotion");
 
-  // const seedFTContract = await seedFT.deploy(team_address, marketing_address);
-  // console.log("CloverDarkSeedToken deployed to:", seedFTContract.address);
-  // const seedFT_address = seedFTContract.address;
+  const seedFTContract = await seedFT.deploy(team_address, marketing_address);
+  console.log("CloverDarkSeedToken deployed to:", seedFTContract.address);
+  const seedFT_address = seedFTContract.address;
 
   const seedPotionContract = await seedPotion.deploy(seedFT_address, marketing_address);
   console.log("CloverDarkSeedPotion deployed to:", seedPotionContract.address);
   const seedPotion_address = seedPotionContract.address;
 
-  // const seedNFTContract = await seedNFT.deploy(seedFT_address);
-  // console.log("CloverDarkSeedNFT deployed to:", seedNFTContract.address);
-  // const seedNFT_address = seedNFTContract.address;
+  const seedNFTContract = await seedNFT.deploy(seedFT_address);
+  console.log("CloverDarkSeedNFT deployed to:", seedNFTContract.address);
+  const seedNFT_address = seedNFTContract.address;
 
-  // const seedControllerContract = await seedController.deploy(team_address, seedFT_address, seedNFT_address, seedPotion_address) ;
-  // console.log("CloverDarkSeedController deployed to:", seedControllerContract.address);
-  // const seedController_address = seedControllerContract.address;
+  const seedControllerContract = await seedController.deploy(team_address, seedFT_address, seedNFT_address, seedPotion_address) ;
+  console.log("CloverDarkSeedController deployed to:", seedControllerContract.address);
+  const seedController_address = seedControllerContract.address;
 
-  // const seedPickerContract = await seedPicker.deploy(seedNFT_address, seedController_address) ;
-  // console.log("CloverDarkSeedPicker deployed to:", seedPickerContract.address);
-  // const seedPicker_address = seedPickerContract.address;
+  const seedPickerContract = await seedPicker.deploy(seedNFT_address, seedController_address) ;
+  console.log("CloverDarkSeedPicker deployed to:", seedPickerContract.address);
+  const seedPicker_address = seedPickerContract.address;
 
-  // const seedStakeContract = await seedStake.deploy(marketing_address, seedFT_address, seedNFT_address, seedController_address, seedPicker_address);
-  // console.log("CloverDarkSeedStake deployed to:", seedStakeContract.address);
+  const seedStakeContract = await seedStake.deploy(marketing_address, seedFT_address, seedNFT_address, seedController_address, seedPicker_address);
+  console.log("CloverDarkSeedStake deployed to:", seedStakeContract.address);
 
 }
 
